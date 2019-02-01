8eac578e6486e04758bf4375ef97050c9576263c
DennySPB <dennyspb@gmail.com>
2019-02-01 12:47:33 -0800

    tulip: refactor some scripts * convert to init.performance.rc * build it * import new tweaks * restore some props

--- device.mk
+++ device.mk
@@ -429,3 +429,19 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+<<<<<<< HEAD
+=======
+
+# QTI Performance
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.performance.rc
+
+# Wi-Fi Display
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+>>>>>>> c3a2ecd... tulip: refactor some scripts
